eb90994c6b468ad58d2c181a28a71de560d9e72c
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-19 09:39:57 -0300

    Remove reactotron configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,14 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { all } from 'redux-saga/effects';
 import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import { persistReducer } from 'redux-persist';
-import Reactotron from 'reactotron-react-js';
 
 import user from '~/store/reducers/user';
 import signed from '~/store/reducers/signed';
 import sagas from '~/store/sagas';
 
-const sagaMiddleware = createSagaMiddleware({
-  sagaMonitor: (() => {
-    if (process.env.NODE_ENV === 'development') {
-      return Reactotron.createSagaMonitor();
-    }
-    return null;
-  })(),
-});
-
+const sagaMiddleware = createSagaMiddleware();
 const persisted = persistReducer(
   {
     key: 'meetapp',
@@ -27,16 +18,7 @@ const persisted = persistReducer(
   combineReducers({ user, signed })
 );
 
-const enhancer = (() => {
-  if (process.env.NODE_ENV === 'development') {
-    return compose(
-      Reactotron.createEnhancer(),
-      applyMiddleware(sagaMiddleware)
-    );
-  }
-  return applyMiddleware(sagaMiddleware);
-})();
-
+const enhancer = applyMiddleware(sagaMiddleware);
 const store = createStore(persisted, enhancer);
 
 sagaMiddleware.run(function* saga() {
