993bad60fbd5c1380509a6ade661c38d1ad9e3a6
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-19 09:41:50 -0300

    Add toast container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { ToastContainer } from 'react-toastify';
+
 import { Theme } from '~/styles/theme';
 import store from '~/store';
 
@@ -10,6 +12,7 @@ export default () => {
     <Provider store={store}>
       <PersistGate persistor={persistStore(store)}>
         <Theme />
+        <ToastContainer />
       </PersistGate>
     </Provider>
   );
