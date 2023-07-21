e4592d093d2592fd21885f4273b7f10453e5b672
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-21 09:10:51 -0300

    Route to sign in

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Redirect, Router, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import SignIn from '~/pages/Sign/In';
 import Header from '~/components/Header';
+import Route from './Route';
 import { Container } from '~/styles/theme';
 
 export default () => {
@@ -12,6 +14,16 @@ export default () => {
     <Router history={history}>
       <Container>
         {signed && <Header />}
+        <Route
+          path="/"
+          exact
+          component={() => {
+            if (signed) {
+              return <Redirect to="/dashboard" />;
+            }
+            return <SignIn />;
+          }}
+        />
       </Container>
     </Router>
   );
