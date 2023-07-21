7c734360210dc38617ea3886a9119faa0def4623
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-21 09:08:06 -0300

    Add page header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Redirect, Router, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+import Header from '~/components/Header';
+import { Container } from '~/styles/theme';
+
 export default () => {
   const signed = useSelector(state => state.signed);
 
   return (
     <Router history={history}>
+      <Container>
+        {signed && <Header />}
+      </Container>
     </Router>
   );
 };
