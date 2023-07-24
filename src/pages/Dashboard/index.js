b34e6c203cff777f562cb3d275299a4f4b1d87a1
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-24 09:53:28 -0300

    Fetch meetups

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+
+import api from '~/services/api';
 import { Container, Header } from './styles';
 
 export default () => {
   const [meetups, setMeetups] = useState([]);
 
   useEffect(() => {
+    (async () => {
+      const response = await api.get('scheduled');
+      setMeetups(
+        response.data.map(meetup => ({
+          ...meetup,
+          formatted_date: format(
+            parseISO(meetup.date),
+            "dd 'de' MMMM', às' H'h'",
+            { locale: pt }
+          ),
+        }))
+      );
+    })();
   }, []);
 
   return (
