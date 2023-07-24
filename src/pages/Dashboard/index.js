5869bcd8f0a2a60a388a44870d567261d03fc2f1
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-24 09:52:44 -0300

    Create dashboard page

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Header } from './styles';
+
+export default () => {
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <Container>
+    </Container>
+  );
+};
