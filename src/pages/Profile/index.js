7cbf96982a85e3e394229cff45fa9230dd4abb0e
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-26 09:26:34 -0300

    Add back button

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { MdArrowBack, MdSave } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
+import { Container } from './styles';
+
 
 export default () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
 
+  return (
+    <Container>
+      <div>
+        <button
+          data-testid="back"
+          type="button"
+          className="unstyled"
+          onClick={() => history.push('/')}
+        >
+          <MdArrowBack color="#FFF" size="24" />
+        </button>
+      </div>
+    </Container>
+  );
 };
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 50px auto;
+  max-width: 940px;
+
+  > div {
+    margin-bottom: 20px;
+  }
+`;
