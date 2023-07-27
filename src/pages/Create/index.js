af245748b7b01fb19057ed1a66a2c3aa3b725a1a
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-27 08:36:40 -0300

    Create meetup creation page

---
+++ src/pages/Create/index.js
@@ -0,0 +1,9 @@
+import React, { useCallback, useState } from 'react';
+import { Container, ImagePicker } from './styles';
+
+export default () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
