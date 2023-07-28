e0da17808eec912adb5dcfef1390d609dfce5a9a
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-28 09:20:25 -0300

    Update banner image

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,7 +1,28 @@
 import React, { useCallback, useState } from 'react';
+import api from '~/services/api';
 import { Container, ImagePicker } from './styles';
 
 export default () => {
+  const [meetup, setMeetup] = useState({});
+  const [preview, setPreview] = useState(null);
+
+  const uploadBanner = useCallback(
+    e => {
+      (async () => {
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+        const response = await api.post('files', data);
+
+        setMeetup({
+          ...meetup,
+          banner_id: response.data.id,
+        });
+        setPreview(response.data.url);
+      })();
+    },
+    [meetup]
+  );
+
   return (
     <Container>
     </Container>
