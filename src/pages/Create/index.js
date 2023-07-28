25509e2ff7e57e6142091bfa0d8f8a0c4479d6bf
Diego Victor <diegovictorgonzaga@gmail.com>
2023-07-28 09:21:30 -0300

    Create schema validation

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,7 +1,20 @@
 import React, { useCallback, useState } from 'react';
 import { MdArrowBack, MdCameraAlt, MdSave } from 'react-icons/md';
+import * as Yup from 'yup';
+
 import api from '~/services/api';
+
 import { Container, ImagePicker } from './styles';
+const schema = Yup.object().shape({
+  date: Yup.date().required('O campo data é obrigatório'),
+  description: Yup.string()
+    .min(10, 'Poxa, explica mais sobre esse meetup ;)')
+    .required('O campo descrição é obrigatório'),
+  localization: Yup.string().required('O campo localização é obrigatório'),
+  title: Yup.string()
+    .min(6, 'O título deve ter no minimo 6 caracteres')
+    .required('O campo título é obrigatório'),
+});
 
 export default () => {
   const [meetup, setMeetup] = useState({});
