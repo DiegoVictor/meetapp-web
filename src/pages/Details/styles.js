e5a47331f55776bc0fe55a1bd88fd812845e7624
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-23 09:59:44 -0300

    Create page styles

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -10,3 +10,56 @@ export const Container = styled.div`
     width: 100%;
   }
 `;
+
+export const Description = styled.div`
+  color: #fff;
+  font-size: 18px;
+  line-height: 32px;
+  margin-bottom: 32px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+
+  h2 {
+    align-items: center;
+    display: flex;
+
+    a {
+      margin-right: 10px;
+      margin-top: 4px;
+    }
+  }
+
+  div {
+    display: flex;
+
+    button,
+    a {
+      margin-left: 15px;
+
+      &.blue {
+        background-color: #4dbaf9;
+      }
+    }
+  }
+`;
+
+export const Footer = styled.div`
+  color: rgba(255, 255, 255, 0.6);
+  display: flex;
+  font-size: 16;
+  line-height: 19px;
+
+  > * {
+    align-items: center;
+    display: flex;
+    margin-right: 30px;
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
