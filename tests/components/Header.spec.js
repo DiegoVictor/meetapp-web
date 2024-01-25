f984e059aebe259b62d87aafe1b390db4f763b36
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-25 08:54:15 -0300

    Test header component

---
+++ tests/components/Header.spec.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { faker } from '@faker-js/faker';
+import { MemoryRouter, Router } from 'react-router-dom';
+
+import history from '~/services/history';
+import { signOut } from '~/store/actions/user';
+import Header from '~/components/Header';
+
+jest.mock('react-redux');
+
+describe('Header component', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockReturnValue(dispatch);
+
+  const user = {
+    name: faker.name.findName(),
+  };
+  useSelector.mockImplementation(cb => {
+    return cb({ user });
+  });
+
+  it('should be able to logout', async () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    await wait(() => expect(getByTestId('logout')).toBeInTheDocument());
+
+    fireEvent.click(getByTestId('logout'));
+
+    expect(dispatch).toHaveBeenCalledWith(signOut());
+  });
+
+  it('should be able to navigate to dashboard', async () => {
+    const { getByTestId } = render(
+      <Router history={history}>
+        <Header />
+      </Router>
+    );
+
+    await wait(() => expect(getByTestId('dashboard')).toBeInTheDocument());
+
+    fireEvent.click(getByTestId('dashboard'));
+
+    expect(history.location.pathname).toBe('/dashboard');
+  });
+
+  it('should be able to see logged in user name', async () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    expect(getByText(user.name)).toBeInTheDocument();
+  });
+});
