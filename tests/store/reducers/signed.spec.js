f60a8f4ca6468dd693a16b43b56a7afe873d3b5f
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-27 09:18:46 -0300

    Create reducers tests

---
+++ tests/store/reducers/signed.spec.js
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker';
+
+import reducer, { initialState } from '~/store/reducers/signed';
+import { signInSuccess, signOut } from '~/store/actions/user';
+import factory from '../../utils/factory';
+
+describe('Signed reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+    expect(state).toBe(initialState);
+  });
+
+  it('SIGN_IN_SUCCESS', async () => {
+    const { name, email } = await factory.attrs('User');
+    const state = reducer(
+      initialState,
+      signInSuccess(faker.random.alphaNumeric(16), { name, email })
+    );
+
+    expect(state).toBe(true);
+  });
+
+  it('SIGN_OUT', () => {
+    const state = reducer(true, signOut());
+    expect(state).toBe(false);
+  });
+});
---
+++ tests/store/reducers/user.spec.js
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker';
+
+import reducer, { initialState } from '~/store/reducers/user';
+import {
+  signInSuccess,
+  updateProfileSuccess,
+  signOut,
+} from '~/store/actions/user';
+import factory from '../../utils/factory';
+
+describe('User reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+    expect(state).toStrictEqual(initialState);
+  });
+
+  it('SIGN_IN_SUCCESS', async () => {
+    const { name, email } = await factory.attrs('User');
+    const token = faker.random.alphaNumeric(16);
+
+    const state = reducer(initialState, signInSuccess(token, { name, email }));
+
+    expect(state).toStrictEqual({ token, name, email });
+  });
+
+  it('UPDATE_PROFILE_SUCCESS', async () => {
+    const { name, email } = await factory.attrs('User');
+    const state = reducer(initialState, updateProfileSuccess({ name, email }));
+
+    expect(state).toStrictEqual({ token: null, name, email });
+  });
+
+  it('SIGN_OUT', () => {
+    const state = reducer(true, signOut());
+    expect(state).toStrictEqual(initialState);
+  });
+});
