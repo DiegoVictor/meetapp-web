2d33d9dbf3e9ee3bea37747434bf35aa73a8f3f2
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-01-29 09:40:43 -0300

    Create and configure estup file

--- jest.config.js
+++ jest.config.js
@@ -126,7 +126,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
