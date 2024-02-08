4a69e782adef292fd963a95b5d6c4949565aedf4
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-02-08 09:45:03 -0300

    Update SignIn page tests

--- tests/pages/SignIn.spec.js
+++ tests/pages/SignIn.spec.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { fireEvent, render, act } from '@testing-library/react';
 
 import { signInRequest } from '~/store/actions/user';
-import history from '~/services/history';
 import { SignIn } from '~/pages/Sign/In';
 import factory from '../utils/factory';
 
-jest.mock('react-redux');
+const mockUseDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockUseDispatch(),
+}));
 
 describe('SignIn page', () => {
   it('should be able to login', async () => {
     const { email, password } = await factory.attrs('User');
 
     const dispatch = jest.fn();
-    useDispatch.mockReturnValue(dispatch);
+    mockUseDispatch.mockReturnValue(dispatch);
 
+    const router = createMemoryRouter([
+      {
+        path: '/',
+        element: <SignIn />,
+      },
+    ]);
     const { getByPlaceholderText, getByTestId } = render(
-      <MemoryRouter>
-        <SignIn />
-      </MemoryRouter>
+      <RouterProvider router={router} />
     );
 
     fireEvent.change(getByPlaceholderText('Digite seu e-mail'), {
@@ -39,13 +45,19 @@ describe('SignIn page', () => {
   });
 
   it('should be able to click on signup link', async () => {
-    const { getByText } = render(
-      <Router history={history}>
-        <SignIn />
-      </Router>
-    );
+    const router = createMemoryRouter([
+      {
+        path: '/',
+        element: <SignIn />,
+      },
+      {
+        path: '/signup',
+        element: <div>SignUp</div>,
+      },
+    ]);
+    const { getByText } = render(<RouterProvider router={router} />);
 
     fireEvent.click(getByText('Criar conta gratuita'));
-    expect(history.location.pathname).toBe('/signup');
+    expect(router.state.location.pathname).toBe('/signup');
   });
 });
